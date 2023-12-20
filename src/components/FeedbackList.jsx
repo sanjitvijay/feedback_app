1b862d4112a50f7e84278ce71add5bd93e77f192
Sanjit Vijay <sanjitv@umich.edu>
2023-12-19 22:11:37 -0500

    Linked to JSON server backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,17 @@ import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length === 0){
+    const {feedback, isLoading} = useContext(FeedbackContext)
+
+    if(!isLoading && (!feedback || feedback.length === 0)){
         return (
             <p>No feedback yet</p>
         )
     }
-    return (
+    return isLoading ? <Spinner/> : (
         <div className="feedback-list">
             <AnimatePresence>
             {feedback.map((item) => (
@@ -28,17 +30,6 @@ function FeedbackList() {
             </AnimatePresence>
         </div>
     )
-
-    // return (
-    //     <div className="feedback-list">
-    //         {feedback.map((item) => (
-    //             <FeedbackItem 
-    //             key={item.id} 
-    //             item={item} 
-    //             handleDelete={handleDelete}/>
-    //         ))}
-    //     </div>
-    // )
 }
 
 
